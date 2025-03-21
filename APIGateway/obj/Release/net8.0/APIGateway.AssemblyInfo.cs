ae063bc7835f79886efe1779d9a237f026c76ea3
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-03-21 12:12:53 +0530

    Minor Code Revisions

--- APIGateway/obj/Release/net8.0/APIGateway.AssemblyInfo.cs
+++ APIGateway/obj/Release/net8.0/APIGateway.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a05a399c-38c2-460f-8486-f808ae2db598")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIGateway")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f81f3a49b865a4f1612d418d3cae2b7cae936d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIGateway")]
